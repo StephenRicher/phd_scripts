59c57eac27ff8537f1e2733e60152302500c6b0d
Stephen Richer <sr467@bath.ac.uk>
2019-05-13 11:59:59 +0100

    Minor syntax fixes and exit on invalid directory.

--- split_fastq.sh
+++ split_fastq.sh
@@ -9,7 +9,7 @@ reads_per_file=10000000
 threads=1
 outdir="./"
 
-while getopts ':n:t:' flag; do
+while getopts ':n:t:o:' flag; do
   case "${flag}" in
     n)  reads_per_file="${OPTARG}" ;;
     t)  threads="${OPTARG}" ;;
@@ -24,14 +24,15 @@ done
 shift "$((OPTIND-1))"
 
 if [[ ! -d "${outdir}" ]]; then
-    >&2 echo "Error "${outdir}" is not a valid directory."
+  >&2 echo "Error "${outdir}" is not a valid directory."
+  exit 1
 fi
 
 split_fastq() {
   zcat -f "${1}" \
-    | split -l $(("${2}"*4)) --filter='gzip > "${3%*/}/"${FILE}".gz' - "${1%.gz}"
+    | split -l $(("${2}"*4)) --filter='gzip > "${3%*/}"/"${FILE}".gz' - "${1%.gz}"
 }
 
 export -f split_fastq
 
-parallel -0 -j "${threads}" split_fastq ::: "${@}" ::: "${reads_per_file}" :: "${outdir}"
+parallel -0 -j "${threads}" split_fastq ::: "${@}" ::: "${reads_per_file}" ::: "${outdir}"
