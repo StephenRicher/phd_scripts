4ae305df7a1ae965f322b577cc690275fc1d75b2
Stephen Richer <sr467@bath.ac.uk>
2019-05-14 16:46:45 +0100

    Sam class print statement and dictionary for filter output.

--- hic_filter.py
+++ hic_filter.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import collections
 import bisect
 import time
@@ -17,6 +19,7 @@ class sam:
         self.tlen = int(sam_line[8])
         self.seq = sam_line[9]
         self.qual = sam_line[10]
+        self.optional = sam_line[11:]
         self.fragment = None
     
     @property    
@@ -61,6 +64,11 @@ class sam:
     @property
     def middle_pos(self):
         return round((self.right_pos + self.left_pos)/2)
+    
+    def print_sam(self):
+        return "{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}".format(
+            self.qname, self.flag, self.rname, self.left_pos, self.mapq, self.cigar,
+            self.rnext, self.pnext, self.tlen, self.seq, self.qual, '\t'.join(map(str,self.optional)))
 
 class fragment:
     def __init__(self, fragment_number, start, end):
@@ -70,7 +78,7 @@ class fragment:
 
 def is_valid(read1, read2):
     if read1.qname != read2.qname:
-        print ("Error: Qname mismatch: {} {} - is file name sorted?". format(read1.qname, read2.qname))
+        print ("Error: Qname mismatch: {} {} - is file name sorted?".format(read1.qname, read2.qname))
     elif not read1.is_paired and not read2.is_paired:
         print("Error: {} is not paired".format(read1.qname))
     elif read1.is_read1 == read2.is_read1:
@@ -82,11 +90,12 @@ def is_valid(read1, read2):
     return False
   
 def is_trans(read1, read2):
+    """ Return true if reads do not align to same reference """
     return read1.rname != read2.rname
     
 def get_fragment(read, digest):
     rf_num = bisect.bisect_left(digest[read.rname], read.middle_pos)
-    rf_start = 1 if rf_num == 0 else digest[read.rname][rf_num -1] + 1
+    rf_start = 1 if rf_num == 0 else digest[read.rname][rf_num - 1] + 1
     rf_end = digest[read.rname][rf_num]
     read.fragment = rf_num
     return fragment(rf_num, rf_start, rf_end)
@@ -149,89 +158,83 @@ def on_adjacent_fragments(read1, read2):
         sys.exit("Error read fragment not set for {}.".format(read1.qname))
     return abs(read1.fragment - read2.fragment) == 1
 
+def run_filter(read1, read2, digest):
+    if not is_valid(read1, read2):
+        status = "invalid_format"
+    else:
+        read1, read2 = reorder_read_pair(read1, read2)
+        orientation = get_orientation(read1, read2)
+        read1_fragment = get_fragment(read1, digest)
+        read2_fragment = get_fragment(read2, digest)
+        ditag_length = tag_length(read1, read1_fragment) + tag_length(read2, read2_fragment)
+        if ditag_length < 150 or ditag_length > 800:
+            status = "wrong_size"
+        elif is_trans(read1, read2):
+            status = "valid_trans"
+        elif on_same_fragment(read1, read2):
+            if orientation == "outward":
+                status = "self_circle"
+            elif orientation == "inward":
+                status = "dangling_end"
+            else:
+                status = "same_strand_error"
+        elif orientation == "inward" and on_adjacent_fragments(read1, read2):
+            status = "religation"
+        elif orientation == "inward" and 150 < read2.five_prime_pos - read1.five_prime_pos + 1 < 800:
+            status = "contiguous"
+        else:
+            status = "valid_cis"
+        return status
+                
+
 def main():
-    
-    digest_filename="Homo_sapiens.GRCh38.dna.primary_assembly_MboI.bed"
+    digest_filename="/home/stephen/x_am/RC-BB1219/stephen_test/genomes/GRCh38/digest/GRCh38_primary_assembly_Mbo1_digest.txt"
     d = process_digest(digest_filename)
-    wrong_size = 0
-    religation = 0
-    contiguous = 0
-    trans = 0
-    valid_cis = 0
-    valid_trans = 0
-    self_circle = 0
-    dangling_end = 0
-    same_strand_error = 0
-    total_pairs = 0
-    invalid_format = 0 
-    contiguous
-    
-    try:
-        with open("HB2_WT_1.fix.pair.sam") as sam_file:
-            for line in sam_file:
-                if line.startswith("@"):
-                    continue
-                else:
+    stats = {"total_pairs": 0, "invalid_format": 0, "wrong_size": 0, "self_circle": 0, "dangling_end": 0, 
+        "same_strand_error": 0, "religation": 0, "contiguous": 0, "valid_cis": 0, "valid_trans": 0}
+        
+    with open("/media/stephen/Data/hic_analysis/data/HB2_CL4-1.fix.pair.sam") as sam_file:
+        for line in sam_file:
+            if line.startswith("@"):
+                continue
+            else:
+                try:
                     read1 = sam(line.split())
                     read2 = sam(next(sam_file).split())
-                    
-                    total_pairs += 1
-                    if not is_valid(read1, read2):
-                        invalid_format += 1
-                        continue
-                    read1, read2 = reorder_read_pair(read1, read2)
-                    orientation = get_orientation(read1, read2)
-                    read1_fragment = get_fragment(read1, d)
-                    read2_fragment = get_fragment(read2, d)
-                    ditag_length = tag_length(read1, read1_fragment) + tag_length(read2, read2_fragment)
-                    if ditag_length < 150 or ditag_length > 800:
-                        wrong_size += 1
-                    elif is_trans(read1, read2):
-                        valid_trans += 1
-                    elif on_same_fragment(read1, read2):
-                        if orientation == "outward":
-                            self_circle += 1
-                        elif orientation == "inward":
-                            dangling_end += 1
-                        else:
-                            same_strand_error += 1
-                    elif orientation == "inward" and on_adjacent_fragments(read1, read2):
-                            religation += 1
-                    elif orientation == "inward" and 150 < read2.five_prime_pos - read1.five_prime_pos + 1 < 800:
-                        contiguous += 1
-                    else:
-                        valid_cis += 1
-                            
-        print("Total pairs", total_pairs)
-        print("Valid pairs", valid_cis + valid_trans)
-        print("Trans", valid_trans)
-        print("Invalid pairs", total_pairs - (valid_cis + valid_trans))
-        print("Self circle", self_circle)
-        print("Dangling end", dangling_end)
-        print("Same strand error", same_strand_error)
-        print("Religation", religation)
-        print("Contiguous", contiguous)
-        print("Invalid format", invalid_format)
-        print("Wrong size", wrong_size)
-                        
-                            
-    except StopIteration:
-        print ("Error: odd number of fastq sequences in file")
+                except StopIteration:
+                    print ("Error: odd number of fastq sequences in file")
+                status = run_filter(read1, read2, d)
+                
+                read1.optional.append("hf:Z:{}".format(status))
+                read2.optional.append("hf:Z:{}".format(status))
+                stats["total_pairs"] += 1
+                stats[status] += 1
+                print(read1.print_sam())
+                print(read1.print_sam())
+        for key,val in stats.items():
+            print (key, "\t", val)
 
+def pysam_test():
+    import pysam
+    samfile = pysam.AlignmentFile("/media/stephen/Data/hic_analysis/data/test.sam", "rb")
+    mysamfile = open("/media/stephen/Data/hic_analysis/data/test2.sam")
+    for read_pysam, read_mysam in zip(samfile.fetch(), mysamfile):
+        myread = sam(read_mysam.split())
+        assert read_pysam.get_reference_positions()[-1] + 1 == myread.right_pos
+        assert read_pysam.get_reference_positions()[0] + 1 == myread.left_pos
+        assert read_pysam.reference_length == myread.reference_length
+        assert read_pysam.is_paired == myread.is_paired
+        assert read_pysam.is_reverse == myread.is_reverse
+        if myread.is_reverse:
+            assert read_pysam.get_reference_positions()[-1] + 1 == myread.five_prime_pos
+            assert read_pysam.get_reference_positions()[0] + 1 == myread.three_prime_pos
+        else:
+            assert read_pysam.get_reference_positions()[-1] + 1 == myread.three_prime_pos
+            assert read_pysam.get_reference_positions()[0] + 1 == myread.five_prime_pos
+    mysamfile.close()
+            
 if __name__ == "__main__":
     main()
-    #import pysam
-    #samfile = pysam.AlignmentFile("/media/stephen/Data/hic_analysis/data/test.sam", "rb")
-    #mysamfile = open("/media/stephen/Data/hic_analysis/data/test.sam")
-    #all_left_pysam = []
-    #all_left_my = []
-    #for read in samfile.fetch():
-    #    all_left_pysam.append(read.get_reference_length()[-1])
-    #for read in mysamfile:
-    #    if not read.startswith("@"):
-    #        myread = sam(read.split())
-    #        all_left_my.append(myread.right_pos)
-    #for i in range(len(all_left_pysam)):
-    #    if all_left_pysam[i] + 1 != all_left_my[i]:
-    #        print(all_left_pysam[i], all_left_my[i])
+    #pysam_test()
+    
 
