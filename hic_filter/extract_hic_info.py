fe499cd7bf0177eabef210e59786124b7f5418b6
Stephen Richer <sr467@bath.ac.uk>
2019-05-31 08:56:16 +0100

    Add import for hic_filter functions.

--- hic_filter/extract_hic_info.py
+++ hic_filter/extract_hic_info.py
@@ -4,9 +4,9 @@
 ### by hic_filter.py. Data is output to stdout in long format for analysis.
 
 import argparse
-
 from sam_class import *
 from smart_read import *
+from hic_filter_functions import *
 
 parser = argparse.ArgumentParser()
 parser.add_argument('file', type = argparse.FileType('r'), help = "Specify sam file.")
@@ -16,7 +16,7 @@ def main():
 
     with smart_read(args.file) as sam_file:
         sys.stdout.write('orientation\tinteraction_type\tditag_length\tinsert_size\tfragment_seperation\n')
-        for line in sam_file:
+        for i, line in enumerate(sam_file):
             if line.startswith("@"):
                 continue
             else:
@@ -28,7 +28,7 @@ def main():
                 if not is_valid(read1, read2):
                     raise ValueError(f'Invalid format in {read1.qname}.') 
                 sys.stdout.write(f"{read1.optional['or:Z']}\t{read1.optional['it:Z']}\t{read1.optional['dt:i']}\t{read1.optional['is:i']}\t{read1.optional['fs:i']}\n")
-
+                
 if __name__ == "__main__":
     main()
 
