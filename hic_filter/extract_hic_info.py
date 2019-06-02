90d0cfd17d0bce0562a976d62d4391ddbaba375e
Stephen Richer <sr467@bath.ac.uk>
2019-06-02 23:06:52 +0100

    Add sample name to header.

--- hic_filter/extract_hic_info.py
+++ hic_filter/extract_hic_info.py
@@ -9,13 +9,20 @@ from smart_read import *
 from hic_filter_functions import *
 
 parser = argparse.ArgumentParser()
+parser.add_argument('--sample', default = None, type = str, help = 'Specify mininum insert size for inward facing read pairs.')
 parser.add_argument('file', type = argparse.FileType('r'), help = "Specify sam file.")
 args = parser.parse_args()
 
 def main():
-
+    
+    if not args.sample:
+        if args.file is not sys.stdin:
+            args.sample = args.file
+        else:
+            args.sample = 'sample'
+        
     with smart_read(args.file) as sam_file:
-        sys.stdout.write('orientation\tinteraction_type\tditag_length\tinsert_size\tfragment_seperation\n')
+        sys.stdout.write('sample\torientation\tinteraction_type\tditag_length\tinsert_size\tfragment_seperation\n')
         for i, line in enumerate(sam_file):
             if line.startswith("@"):
                 continue
@@ -27,7 +34,7 @@ def main():
                     sys.exit("Error: odd number of alignments in file")
                 if not is_valid(read1, read2):
                     raise ValueError(f'Invalid format in {read1.qname}.') 
-                sys.stdout.write(f"{read1.optional['or:Z']}\t{read1.optional['it:Z']}\t{read1.optional['dt:i']}\t{read1.optional['is:i']}\t{read1.optional['fs:i']}\n")
+                sys.stdout.write(f"{args.sample}\t{read1.optional['or:Z']}\t{read1.optional['it:Z']}\t{read1.optional['dt:i']}\t{read1.optional['is:i']}\t{read1.optional['fs:i']}\n")
                 
 if __name__ == "__main__":
     main()
