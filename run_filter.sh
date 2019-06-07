cb4bb0cf3efbccd908f110b87d3bbab90194350a
Stephen Richer <sr467@bath.ac.uk>
2019-06-07 13:57:37 +0100

    Add command line arguments.

--- run_filter.sh
+++ run_filter.sh
@@ -4,30 +4,31 @@ filter_reads="/home/stephen/h/phd/scripts2/hic_scripts/hic_filter/filter_reads.p
 extract_hic_info="/home/stephen/h/phd/scripts2/hic_scripts/hic_filter/extract_hic_info.py"
 filter_by_region="/home/stephen/h/phd/scripts2/hic_scripts/filter_by_region.txt"
 hicup_to_hicexplorer="/home/stephen/h/phd/scripts2/hic_scripts/hicup_to_hicexplorer.txt"
-
+hicexplorer_normalize="/home/stephen/phd/scripts/hicexplorer_normalize.sh"
 capture_regions="/home/stephen/h/phd/scripts2/hic_scripts/capture_regions.bed"
 genome="/home/stephen/x_db/DBuck/s_richer/stephen_test/genomes/GRCh38/wgs/Homo_sapiens.GRCh38.dna.primary_assembly.fa"
 
-for bam in *.name_sorted.bam; do
+for bam in "${@}"; do
   (
   sample="${bam%%.*}"; sample="${sample##*/}"
-  samtools view -h "${bam}" | "${filter}" -d "${digest_file}" - | samtools view -Sb > "${bam/name_sorted/hic_info}"
+  samtools view -h "${bam}" | "${filter}" -d "${digest_file}" - | samtools view -Sb > "${sample}".hic_info.bam
   # Sample 10% of reads and extract HiC stats.
-  samtools view -s 42.05 "${bam/name_sorted/hic_info}" | "${extract_hic_info}" --sample "${sample}" - >> all_samples.hic_info_sampled.txt
+  samtools view -s 42.05 "${sample}".hic_info.bam | "${extract_hic_info}" --sample "${sample}" - >> all_samples.hic_info_sampled.txt
   ) &
 done; wait
 
 # Remove all header from all_samples file and add 1 to the top of the file.
-cat <(head -n 1 all_samples.hic_info_sampled.txt) <(grep -vP "sample\torientation" all_samples.hic_info_sampled.txt) > all_samples.hic_info_sampled.tmp.txt
-mv all_samples.hic_info_sampled.tmp.txt all_samples.hic_info_sampled.txt
+#cat <(head -n 1 all_samples.hic_info_sampled.txt) <(grep -vP "sample\torientation" all_samples.hic_info_sampled.txt) > all_samples.hic_info_sampled.tmp.txt
+#mv all_samples.hic_info_sampled.tmp.txt all_samples.hic_info_sampled.txt
 
-for bam in *.name_sorted.bam; do
-  echo Processing "${bam}"
-  samtools view -@ 6 -h "${bam/name_sorted/hic_info}" \
+for bam in "${@}"; do
+  sample="${bam%%.*}"; sample="${sample##*/}"
+  echo Processing "${sample}".hic_info.bam
+  samtools view -@ 6 -h "${sample}".hic_info.bam \
     | "${filter_reads}" --min_inward 1000 --max_ditag 1000 - \
-    | samtools view -@ 6 -b > "${bam/name_sorted/filtered}"
-  samtools sort -@ 3 "${bam/name_sorted/filtered}" > "${bam/name_sorted/filtered-sort}"
-  samtools index -@ 3 "${bam/name_sorted/filtered-sort}"
+    | samtools view -@ 6 -b > "${sample}".filtered.bam
+  samtools sort -@ 3 "${sample}".filtered.bam > "${sample}".filtered-sort.bam
+  samtools index -@ 3 "${sample}".filtered-sort.bam
 done
 
 "${filter_by_region}" "${capture_regions}" "${genome}" 12 . *filtered-sort.bam
@@ -49,7 +50,7 @@ fi
 count=0
 for bam in *filtered-sort.bam; do
   ((count++))
-  "${hicup_to_hicexplorer}" -f "${bam}" -c "${capture_regions}" -t 1 -o "${hcx_dir}" -d "${hcx_dir}"/"${genome_nopath%.*}"_"${enzyme}"_rest_site.bed
+  "${hicup_to_hicexplorer}" -f "${bam}" -c "${capture_regions}" -t 6 -o "${hcx_dir}" -d "${hcx_dir}"/"${genome_nopath%.*}"_"${enzyme}"_rest_site.bed
 
   # Combine all summary files. If first in loop then include header.
   if [ "${count}" -eq "1" ]; then
@@ -58,4 +59,15 @@ for bam in *filtered-sort.bam; do
     tail -n +2 "${hcx_dir}"/"${bam%%.*}"-summary.txt >> "${hcx_dir}"/all_samples_summary.txt
   fi
 done
+
+hicexplorer_normalize="/home/stephen/phd/scripts/hicexplorer_normalize.sh"
+n=6
+while IFS=$'\t' read -r chr start end region; do
+  ((i=i%n)); ((i++==0)) && wait
+  "${hicexplorer_normalize}" "${region}" "${chr}" "${start}" "${end}" "${hcx_dir}"/all_regions/"${region}" "allele" &
+done <${capture_regions}; wait
+shutdown
+
+
+
 
