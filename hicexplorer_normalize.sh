137aab5e8e17207662e3350c58df5d1f58c38fdd
Stephen Richer <sr467@bath.ac.uk>
2019-06-14 14:19:48 +0100

    Improvements to TAD calling with onTAD.

--- hicexplorer_normalize.sh
+++ hicexplorer_normalize.sh
@@ -4,16 +4,35 @@
 shopt -s extglob
 
 region="${1}"
-location="${2}":"${3}"-"${4}"
 dir="${5}"
+chr="${2}"
+start="${3}"
+end="${4}"
+
+# Define plot region - set maximum plot region size
+max_size=1500000
+if (( $((end - start)) > "${max_size}" )); then
+  mid=$(((end + start)/2))
+  start_plot=$((mid-(max_size/2)))
+  end_plot=$((mid+(max_size/2)))
+  location="${chr}":"${start_plot}"-"${end_plot}"
+else
+  location="${chr}":"${start}"-"${end}"
+fi
+
+
+
 
 if [[ "${6}" == "allele" ]]; then
   tracks=("/home/stephen/x_am/RC-BB1219/stephen/projects/hic_analysis/hicexplorer_tracks/hic_track_genome_split.ini")
   samples=("HB2_CL4_G1" "HB2_CL4_G2" "HB2_WT_G1" "HB2_WT_G2" "MCF7_G1" "MCF7_G2")
 else
   samples=("HB2_CL4" "HB2_WT" "MCF7")
-  tracks=("/home/stephen/x_am/RC-BB1219/stephen/projects/hic_analysis/hicexplorer_tracks/hic_track.ini" \
-          "/home/stephen/x_am/RC-BB1219/stephen/projects/hic_analysis/hicexplorer_tracks/hic_track_replicate.ini")
+  #tracks=("/home/stephen/x_am/RC-BB1219/stephen/projects/hic_analysis/hicexplorer_tracks/hic_track.ini" \
+  #        "/home/stephen/x_am/RC-BB1219/stephen/projects/hic_analysis/hicexplorer_tracks/hic_track_replicate.ini" \
+  #        "/home/stephen/x_am/RC-BB1219/stephen/projects/hic_analysis/hicexplorer_tracks/hic_track_replicate_ontad.ini")
+  tracks=("/home/stephen/x_am/RC-BB1219/stephen/projects/hic_analysis/hicexplorer_tracks/hic_track_ontad.ini" \
+          "/home/stephen/x_am/RC-BB1219/stephen/projects/hic_analysis/hicexplorer_tracks/hic_track_replicate_ontad.ini")
 fi
 
 # Check hic interaction density for each replicate of each sample. If any are 0 then remove from sample list
@@ -58,7 +77,8 @@ done
 # Note matrices_norm (used below) is defined for 1000 bin size matrices after for loop.
 # Merge matrices at varying bin sizes and generate diagnostic plots to view bin read count distribution.
 mkdir -p "${dir}"/correlation_plots "${dir}"/diagnostic_plots
-for nbin in rf $(seq 1000 1000 20000) $(seq 30000 10000 50000); do
+for nbin in $(seq 1000 1000 20000) $(seq 30000 10000 50000) rf; do
+
   mkdir -p "${dir}"/"${nbin}"
   for matrix in "${matrices_norm[@]}"; do
     matrix_rmpath="${matrix##*/}"; matrix_rmpath="${matrix_rmpath/'-1000-'/-${nbin}-}"
@@ -76,14 +96,50 @@ for nbin in rf $(seq 1000 1000 20000) $(seq 30000 10000 50000); do
     hicSumMatrices --matrices "${dir}"/"${nbin}"/"${sample}"*-norm.h5 --outFileName "${dir}"/"${nbin}"/"${sample}"-"${region}"-"${nbin}"-norm_sum.h5
   done
 
+  ontad="/home/stephen/Downloads/OnTAD-master/src/OnTAD"
+  mkdir -p "${dir}"/"${nbin}"/tads
   # Correct merged and unmerged matrices
   for matrix in "${dir}"/"${nbin}"/*-norm*(_sum).h5; do
-    hicCorrectMatrix diagnostic_plot --matrix ${matrix} -o diagnostic_plot_temp.png 2> diagnostic_temp.txt
-    lower_threshold=$(grep "mad threshold" diagnostic_temp.txt | awk '{print $NF}')
+    matrix_rmpath="${matrix##*/}"
+    hicCorrectMatrix diagnostic_plot --matrix ${matrix} -o "${dir}"/diagnostic_plots/${matrix_rmpath%.h5}_diagnostic_temp.png \
+      2> "${dir}"/diagnostic_plots/${matrix_rmpath%.h5}_diagnostic_log_temp.txt
+    lower_threshold=$(grep "mad threshold" "${dir}"/diagnostic_plots/${matrix_rmpath%.h5}_diagnostic_log_temp.txt | awk '{print $NF}')
     hicCorrectMatrix correct --matrix "${matrix}" --correctionMethod ICE --iterNum 1000 \
                              --filterThreshold ${lower_threshold} 5 --outFileName "${matrix%.h5}"_iced.h5
-    rm diagnostic_temp.txt diagnostic_plot_temp.png
+    rm "${dir}"/diagnostic_plots/${matrix_rmpath%.h5}_diagnostic_log_temp.txt 
+    rm "${dir}"/diagnostic_plots/${matrix_rmpath%.h5}_diagnostic_temp.png
+    if [[ "${nbin}" != "rf" ]]; then
+      hicConvertFormat --matrices "${matrix%.h5}"_iced.h5 --inputFormat h5 --outputFormat homer \
+                       --outFileName "${matrix%.h5}"_iced.homer.gz
+      zcat "${matrix%.h5}"_iced.homer.gz | cut -f3- | tail -n+2 > "${matrix%.h5}"_iced.nn.txt
+      "${ontad}" "${matrix%.h5}"_iced.nn.txt -maxsz $((1000000/nbin)) -o "${dir}"/"${nbin}"/tads/"${matrix_rmpath%.h5}"_iced -bedout chr"${chr}" "${nbin}"
+      #tail -n+2 "${dir}"/"${nbin}"/tads/"${matrix_rmpath%.h5}"_iced.bed \
+      #  | awk -v start="${start}" -v OFS='\t' '{$2=$2+start; $3=$3+start; $7=$7+start; $8=$8+start; print}' \
+      #  > "${dir}"/"${nbin}"/tads/"${matrix_rmpath%.h5}"_iced.tmp.bed
+      tail -n+2 "${dir}"/"${nbin}"/tads/"${matrix_rmpath%.h5}"_iced.bed \
+        | awk -v start="${start}" -v OFS='\t' '
+            {$2=$2+start; $3=$3+start; $7=$7+start; $8=$8+start; print $1, $2, $3, $1, $7, $8, 0}' \
+        > "${dir}"/"${nbin}"/tads/"${matrix_rmpath%.h5}"_iced.links
+      hicConvertFormat --matrices "${matrix%.h5}"_iced.h5 --inputFormat h5 --outputFormat cool \
+                       --outFileName "${matrix%.h5}"_iced.cool --resolutions "${nbin}"
+    fi
   done
+
+  # Iterate through replicate pairs and detect hierarchical TADs using hitad
+  #if [[ "${nbin}" != "rf" ]]; then
+  #  mkdir -p "${dir}"/"${nbin}"/tads
+  #  for sample in "${samples[@]}"; do
+  #    echo res:"${nbin}" > "${dir}"/"${nbin}"/tads/"${sample}"_hitad_metafile.txt
+  #    r=1
+  #    for replicate in "${dir}"/"${nbin}"/"${sample}"*-norm_iced.cool; do
+  #      echo rep"${r}":"${replicate}" >> "${dir}"/"${nbin}"/tads/"${sample}"_hitad_metafile.txt
+  #      ((r++))
+  #    done
+  #    hitad --output "${dir}"/"${nbin}"/tads/"${sample}"_hitad.txt \
+  #          --datasets "${dir}"/"${nbin}"/tads/"${sample}"_hitad_metafile.txt \
+  #          --logFile "${dir}"/"${nbin}"/tads/"${sample}"_hitad_log.txt
+  #  done 
+  #fi 
 
   mkdir -p "${dir}"/"${nbin}"/matrix_comparison/
   for matrix1 in "${dir}"/"${nbin}"/*-norm_sum_iced.h5; do
@@ -141,11 +197,11 @@ for nbin in rf $(seq 1000 1000 20000) $(seq 30000 10000 50000); do
     if [ -f "${loops_file}" ]; then
       hicPlotMatrix --matrix ${matrix} --outFileName "${dir}"/"${nbin}"/hic_plots/${matrix_rmpath/.h5}.png \
                     --log1p --region ${location} --loops "${loops_file}" \
-                    --title ${matrix/.h5} --dpi 300 --vMin 1 --vMax ${vMax}
+                    --title ${matrix_rmpath/.h5} --dpi 300 --vMin 1 --vMax ${vMax}
     else
       hicPlotMatrix --matrix ${matrix} --outFileName "${dir}"/"${nbin}"/hic_plots/${matrix_rmpath/.h5}.png \
                     --log1p --region ${location} \
-                    --title ${matrix/.h5} --dpi 300 --vMin 1 --vMax ${vMax}
+                    --title ${matrix_rmpath/.h5} --dpi 300 --vMin 1 --vMax ${vMax}
     fi
   done
 
@@ -154,15 +210,19 @@ for nbin in rf $(seq 1000 1000 20000) $(seq 30000 10000 50000); do
 
   mkdir -p "${dir}"/"${nbin}"/tads
   mkdir -p "${dir}"/"${nbin}"/tad_plots/tracks
-  for delta in 0.001 0.005 0.01 0.05; do
-    for threshold in 0.001 0.005 0.01 0.05; do
+
+  delta=0.001
+  threshold=0.001
+
+  #for delta in 0.001 0.005 0.01 0.05; do
+  #  for threshold in 0.001 0.005 0.01 0.05; do
 
       # Detect TADS from both summed and unsummed restriction fragment resolution and merged resolution HiC matrices
-      for matrix in "${dir}"/"${nbin}"/*-norm_?(sum_)iced.h5; do
-        matrix_rmpath="${matrix##*/}"
-        hicFindTADs --matrix "${matrix}" --outPrefix "${dir}"/"${nbin}"/tads/${matrix_rmpath/-norm_*iced.h5}-TADS_d_${delta}_t_${threshold} \
-                    --correctForMultipleTesting fdr --thresholdComparisons ${threshold} --delta ${delta}
-      done
+      #for matrix in "${dir}"/"${nbin}"/*-norm_?(sum_)iced.h5; do
+      #  matrix_rmpath="${matrix##*/}"
+      #  hicFindTADs --matrix "${matrix}" --outPrefix "${dir}"/"${nbin}"/tads/${matrix_rmpath/-norm_*iced.h5}-TADS_d_${delta}_t_${threshold} \
+      #              --correctForMultipleTesting fdr --thresholdComparisons ${threshold} --delta ${delta}
+      #done
 
       for track in "${tracks[@]}"; do
       
@@ -195,11 +255,9 @@ for nbin in rf $(seq 1000 1000 20000) $(seq 30000 10000 50000); do
         # Replot the graph.
         hicPlotTADs --tracks "${plot_track}" --region ${location} --outFileName "${plotname}" --title ${region}_delta_${delta}_threshold_${threshold} --dpi 300 
       done
-    done
-  done
+    #done
+  #done
   # Remove temporary files
   rm "${dir}"/"${nbin}"/tad_plots/${track##*/}_plot_log_temp.txt
 done
-
-
 
