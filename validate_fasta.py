85e613b08dc9bff9c5035526369d2bf88dbd832b
Stephen Richer <sr467@bath.ac.uk>
2019-07-10 11:26:26 +0100

    Improved command line parameters and error logging.

--- validate_fasta.py
+++ validate_fasta.py
@@ -2,39 +2,41 @@
 
 import re, argparse, sys, gzip, contextlib, os, stat, select
 
-from smart_open import smart_open
-
 parser = argparse.ArgumentParser()
-parser.add_argument("-f", "--file", help = "Specify input file name.", nargs = "?")
+parser.add_argument('-f', '--file', nargs = '?', help = 'Specify input file name.', type = argparse.FileType('r'), default = sys.stdin)
+parser.add_argument('-l', '--log', nargs = '?', help = 'Specify log file name', type = argparse.FileType('w'),  default = sys.stderr)
 args = parser.parse_args()
-
-def eprint(*args, **kwargs):
-    """ Print to stderr and exit """
-    print(*args, file=sys.stderr, **kwargs)
-    sys.exit(1)
 
 if __name__ == '__main__':
     if not args.file and sys.stdin.isatty():
         parser.print_help()
 
-    with smart_open(args.file, mode = "read") as f:
-        
+    invalid = False
+
+    with args.file as f, args.log as log::
         for index, line in enumerate(f):
             line = line.rstrip('\n')
-            in_header = True if line.startswith(">") else False
+            in_header = True if line.startswith('>') else False
             header_above = None
             if not line:
-                eprint("Error: Line {} is a blank line.".format(index))
+                log.write(f'Error: Line {index} is a blank line.')
+                invalid = True
             elif not in_header:
-                invalid_characters = re.search("[^ATCGURYKMSWBDHVN-]", line, re.IGNORECASE)
+                invalid_characters = re.search('[^ATCGURYKMSWBDHVN-]', line, re.IGNORECASE)
                 if invalid_characters:
-                    eprint("Error: Invalid sequence character '{}' at line {}.".format(invalid_characters.group(0), index))
+                    log.write(f'Error: Invalid sequence character "{invalid_characters.group(0)}" at line {index}.')
+                    invalid = True
                 elif in_header == header_above:
-                    eprint("Error: FASTA sequence contains line wrapping.")
+                    log.write('Error: FASTA sequence contains line wrapping.')
+                    invalid = True
             # If we are in header and the previous line was also a header...
             elif in_header == header_above:
-                eprint("Error: Header missing sequence at line {}.".format(index))
+                log.write(f'Error: Header missing sequence at line {index}.')
+                invalid = True
             header_above = in_header
         if in_header:
-            eprint("Error: Header missing sequence on last last.")
+            log.write('Error: Header missing sequence on last last.')
+            invalid = True
+        if invalid:
+            sys.exit(1)
         
