8a58c53647aa41bc3079387aa9d321e49199efd2
Stephen Richer <sr467@bath.ac.uk>
2019-07-11 16:37:37 +0100

    Added logging functionality.

--- hic_filter/extract_hic_info.py
+++ hic_filter/extract_hic_info.py
@@ -3,7 +3,7 @@
 ### Script to extract the HiC read pair information encoding in the SAM optional field 
 ### by hic_filter.py. Data is output to stdout in long format for analysis.
 
-import argparse
+import sys, argparse, logging
 
 sys.path.insert(0, '/home/stephen/h/phd/scripts2/hic_scripts/hic_filter/')
 
@@ -14,31 +14,48 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-s', '--sample', help = 'Specify sample name.', type = str, default = None)
 parser.add_argument('-f', '--file', nargs = '?', help = 'Specify sam file.', type = argparse.FileType('r'), default = sys.stdin)
 parser.add_argument('-o', '--out', nargs = '?', help = 'Specify output file name', type = argparse.FileType('w'),  default = sys.stdout)
+parser.add_argument('-l', '--log', nargs = '?', help = 'Specify log file name', type = argparse.FileType('w'),  default = sys.stderr)
 args = parser.parse_args()
 
-def main():
-    
-    if not args.sample:
-        if args.file is not sys.stdin:
-            args.sample = args.file
-        else:
-            args.sample = 'sample'
-        
-    with args.file as sam_file, args.out as out:
-        out.write('sample\torientation\tinteraction_type\tditag_length\tinsert_size\tfragment_seperation\n')
-        for i, line in enumerate(sam_file):
-            if line.startswith('@'):
-                continue
+def handle_exception(exc_type, exc_value, exc_traceback):
+    ''' Handle uncaught exceptions (excluding KeyboardInterrupt) 
+        through the logging module. '''
+    if issubclass(exc_type, KeyboardInterrupt):
+        sys.__excepthook__(exc_type, exc_value, exc_traceback)
+        return
+    logging.getLogger(__name__).critical("Uncaught exception", exc_info = (exc_type, exc_value, exc_traceback))
+
+sys.excepthook = handle_exception
+
+logging.basicConfig(stream = args.log, format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s', level = logging.DEBUG)
+log = logging.getLogger(__name__)
+
+def run():
+    try:
+        if not args.sample:
+            if args.file is not sys.stdin:
+                args.sample = args.file
             else:
-                try:
-                    read1 = sam(line.split())
-                    read2 = sam(next(sam_file).split())
-                except StopIteration:
-                    sys.exit('Error: odd number of alignments in file')
-                if not is_valid(read1, read2):
-                    raise ValueError(f'Invalid format in {read1.qname}.') 
-                out.write(f"{args.sample}\t{read1.optional['or:Z']}\t{read1.optional['it:Z']}\t{read1.optional['dt:i']}\t{read1.optional['is:i']}\t{read1.optional['fs:i']}\n")
-                
+                args.sample = 'sample'
+        
+        with args.file as sam_file, args.out as out:
+            out.write('sample\torientation\tinteraction_type\tditag_length\tinsert_size\tfragment_seperation\n')
+            for i, line in enumerate(sam_file):
+                if line.startswith('@'):
+                    continue
+                else:
+                    try:
+                        read1 = sam(line.split())
+                        read2 = sam(next(sam_file).split())
+                    except StopIteration:
+                        log.exception('Odd number of alignments in file')
+                        sys.exit(1)
+                    if not is_valid(read1, read2):
+                        log.error(f'Invalid format in {read1.qname}.')
+                    out.write(f"{args.sample}\t{read1.optional['or:Z']}\t{read1.optional['it:Z']}\t{read1.optional['dt:i']}\t{read1.optional['is:i']}\t{read1.optional['fs:i']}\n")
+    finally:
+        logging.shutdown()
+        
 if __name__ == "__main__":
-    main()
+    run()
 
