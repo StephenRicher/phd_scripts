341c886ac389264f35f25eb19756ac6dae3c3f96
Stephen Richer <sr467@bath.ac.uk>
2019-07-11 16:41:08 +0100

    Added logging functionality.

--- hic_filter/hic_filter.py
+++ hic_filter/hic_filter.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import argparse, contextlib, collections, bisect, time, re, sys, math, logging
+import argparse, collections, bisect, time, re, sys, math, logging
 
 sys.path.insert(0, '/home/stephen/h/phd/scripts2/hic_scripts/hic_filter/')
 
@@ -10,38 +10,56 @@ from hic_filter_functions import *
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', '--file', nargs = '?', help = 'Specify sam file.', type = argparse.FileType('r'), default = sys.stdin)
 parser.add_argument('-o', '--out', nargs = '?', help = 'Specify output file name', type = argparse.FileType('w'),  default = sys.stdout)
+parser.add_argument('-l', '--log', nargs = '?', help = 'Specify log file name', type = argparse.FileType('w'),  default = sys.stderr)
 # Create group for required named arguments.
 requiredNamed = parser.add_argument_group('required named arguments')
-requiredNamed.add_argument('-d', '--digest', help = 'Specify digest file.', required = True)
+requiredNamed.add_argument('-d', '--digest', help = 'Specify digest file.', type = argparse.FileType('r'), required = True)
 args = parser.parse_args()
 
-def main():
-    d = process_digest(args.digest)
+def handle_exception(exc_type, exc_value, exc_traceback):
+    ''' Handle uncaught exceptions (excluding KeyboardInterrupt) 
+        through the logging module. '''
+    if issubclass(exc_type, KeyboardInterrupt):
+        sys.__excepthook__(exc_type, exc_value, exc_traceback)
+        return
+    logging.getLogger(__name__).critical("Uncaught exception", exc_info = (exc_type, exc_value, exc_traceback))
 
-    with args.file as sam_file, args.out as out:
-        for line in sam_file:
-            if line.startswith("@"):
-                out.write(line)
-                continue
-            else:
-                try:
-                    read1 = sam(line.split())
-                    read2 = sam(next(sam_file).split())
-                except StopIteration:
-                    sys.exit("Error: odd number of alignments in file")
-                orientation, ditag_length, insert_size, interaction, fragment_seperation = run_filter(read1, read2, d)
-                read1.optional['or:Z'] = orientation
-                read2.optional['or:Z'] = orientation
-                read1.optional['it:Z'] = interaction
-                read2.optional['it:Z'] = interaction
-                read1.optional['dt:i'] = ditag_length
-                read2.optional['dt:i'] = ditag_length
-                read1.optional['is:i'] = insert_size
-                read2.optional['is:i'] = insert_size
-                read1.optional['fs:i'] = fragment_seperation
-                read2.optional['fs:i'] = fragment_seperation
-                out.write(read1.print_sam())
-                out.write(read2.print_sam())
+sys.excepthook = handle_exception
+
+logging.basicConfig(stream = args.log, format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s', level = logging.DEBUG)
+log = logging.getLogger(__name__)
+
+def run():
+    try:
+        with args.file as sam_file, args.out as out, args.digest as digest:
+            d = process_digest(digest)
+            for line in sam_file:
+                if line.startswith("@"):
+                    out.write(line)
+                    continue
+                else:
+                    try:
+                        read1 = sam(line.split())
+                        read2 = sam(next(sam_file).split())
+                    except StopIteration:
+                        log.exception("Odd number of alignments in file")
+                    orientation, ditag_length, insert_size, interaction, fragment_seperation, read1_fragment, read2_fragment = run_filter(read1, read2, d)
+                    read1.optional['or:Z'] = orientation
+                    read2.optional['or:Z'] = orientation
+                    read1.optional['it:Z'] = interaction
+                    read2.optional['it:Z'] = interaction
+                    read1.optional['dt:i'] = ditag_length
+                    read2.optional['dt:i'] = ditag_length
+                    read1.optional['is:i'] = insert_size
+                    read2.optional['is:i'] = insert_size
+                    read1.optional['fs:i'] = fragment_seperation
+                    read2.optional['fs:i'] = fragment_seperation
+                    read1.optional['fn:i'] = read1_fragment
+                    read2.optional['fn:i'] = read2_fragment
+                    out.write(read1.print_sam())
+                    out.write(read2.print_sam())
+    finally:
+        logging.shutdown()
                 
 def pysam_test():
     import pysam
@@ -63,7 +81,7 @@ def pysam_test():
     mysamfile.close()
             
 if __name__ == "__main__":
-    main()
+    run()
     #pysam_test()
     
 
