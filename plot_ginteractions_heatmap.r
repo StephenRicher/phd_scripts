33389c6c958f7a7459a9c973dd358e61b234309d
Stephen Richer <sr467@bath.ac.uk>
2019-08-15 16:44:36 +0100

    Created plotly graph rather than ggplot.

--- plot_ginteractions_heatmap.r
+++ plot_ginteractions_heatmap.r
@@ -3,11 +3,13 @@ library(ggthemes)
 library(plotly)
 library(reshape2)
 data = read.table('~/x_db/DBuck/s_richer/stephen_test/projects/hic_analysis/post_alignment/hicexplorer/all_regions/GNG12_AS1_DIRAS3/5000/HB2_WT_1-GNG12_AS1_DIRAS3-5000-norm_iced_obs_exp.ginteractions.tsv')
+data$V7 = ifelse(data$V7 > 2, 2, data$V7)
 
+data = data[data$V2 > 67000000 & data$V5 <  69000000,]
 
 a = dcast(data, V2 ~ V5, value.var = "V7")
+rownames(a) = a[,1]
+a = as.matrix(a[,-1])
 
-
-p <- ggplot(data, aes(V2, V5)) + geom_tile(aes(fill = V7)) + scale_fill_gradient2(limits=c(-0,2), midpoint = 1)
-# scale_fill_gradient2_tableau(palette = "Orange-Blue Diverging", na.value = "grey50", guide = "colourbar")
-ggplotly(p)
+p <- plot_ly(x = rownames(a), y = colnames(a), z = t(a), colors = "PiYG", type = "heatmap")
+p
