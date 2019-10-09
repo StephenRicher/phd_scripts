d164bccd414ba0b0525e292a9e6274220add74b6
Stephen Richer <sr467@bath.ac.uk>
2019-10-09 16:27:29 +0100

    Mask genome at SNP sites.

---
+++ allele_specific/masked_genome.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+shopt -s extglob
+
+print_usage() {
+  echo "Usage: ..."
+}
+
+# Define default parameters that may be optionally specified
+out=".";
+threads=1
+
+while getopts 's:g:v:o:t:' flag; do
+  case "${flag}" in
+    s) sample="${OPTARG}" ;;
+    g) genome="${OPTARG}" ;;
+    v) vcf="${OTARG}" ;;
+    o) out="${OPTARG}" ;;
+    t) threads="${OPTARG}" ;;
+    *) print_usage
+       exit 1 ;;
+  esac
+done
+
+# If genome is not indexed then raise error.
+if [ ! -f "${genome}".fai ] ; then
+  echo "Genome must be indexed."; exit 
+fi
+
+## Masked reference genome at SNPs and build bowtie2 index ##
+
+genome_rmpath="${genome##*/}"
+masked_genome="${out}"/"${genome_rmpath%.fa?(.gz)}"_"${sample}"_masked.fa
+
+bedtools maskfasta \
+  -fullHeader \
+  -fi <(zcat -f "${genome}") \
+  -bed "${vcf}" \
+  -fo "${masked_genome}"
+
+bowtie2-build \
+  --threads "${threads}" \
+  "${masked_genome}" \
+  "${out}"/GRCh38_"${sample}" 
