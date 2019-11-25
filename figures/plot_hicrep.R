202d547b89174a3da8b65f44f596870826130010
Stephen Richer <sr467@bath.ac.uk>
2019-11-25 14:12:01 +0000

    Updated to include allele specific functionality.

--- figures/plot_hicrep.R
+++ figures/plot_hicrep.R
@@ -1,16 +1,29 @@
+#!/usr/bin/Rscript
+
 library(hicrep)
 library(pheatmap)
 library(reshape2)
 library(RColorBrewer)
 library(igraph)
 library(ggplot2)
+library(viridisLite)
 
-data_dir = "/media/stephen/Data/hic_analysis_v2/hicexplorer/all_regions"
+args = commandArgs(trailingOnly=TRUE)
+
+if (args[1] == 'allele') {
+  samples = c("HB2_WT_G1-1", "HB2_WT_G1-2", "HB2_WT_G2-1", "HB2_WT_G2-2")
+  qc_dir = '/home/stephen/x_db/DBuck/s_richer/hic_01/allele_specific/qc'
+  data_dir = '/home/stephen/x_db/DBuck/s_richer/hic_01/allele_specific/hicexplorer/all_regions'
+  binrange = seq(5000, 20000, 1000) 
+} else {
+  samples = c("HB2_WT-1", "HB2_WT-2", "HB2_TSS-KO 1", "HB2_TSS-KO-2", "MCF7-1", "MCF7-2")
+  qc_dir = '/home/stephen/x_db/DBuck/s_richer/hic_01/qc/'
+  data_dir = '/home/stephen/x_db/DBuck/s_richer/hic_01/data/hicexplorer/all_regions/'
+  binrange = seq(1000, 10000, 1000)
+}
 
 # All hicrep output will be stored in a folder in qc directory
-qc_dir = "/home/stephen/x_db/DBuck/s_richer/stephen_test/projects/hic_analysis2/qc"
 dir.create(paste(qc_dir, 'hicrep', sep = '/'))
-
 capture_regions = read.csv("/home/stephen/phd/scripts/capture_regions.bed", sep = "\t", 
                            header = FALSE, col.names = c("chromosome", "start", "end", "region"))
 
@@ -19,11 +32,6 @@ scc_values <- data.frame("region" = character(0), "binsize" = numeric(0),
                          "sample1" = character(0), "sample2" = character(0),
                          "h" = numeric(0), "scc" = numeric(0),
                          stringsAsFactors=FALSE)
-
-# Define sample names with replicate number
-samples = c("HB2_WT-1", "HB2_WT-2", "HB2_TSS-KO 1", "HB2_TSS-KO-2", "MCF7-1", "MCF7-2")
-
-binrange = seq(1000, 10000, 1000)
 
 for (region in capture_regions$region) {
   
@@ -46,7 +54,7 @@ for (region in capture_regions$region) {
       m1 = read.csv(m1_path, sep = '\t', header = FALSE)
       for (sample2 in samples) {
         if (sample1 != sample2) {
-          next
+          #next
           m2_path = paste(
             data_dir, "/", region, "/", bin, "/", sample2, "-", region, "-", bin, "_hicrep.tsv", 
             sep = "")
@@ -55,18 +63,16 @@ for (region in capture_regions$region) {
             next
           }
           m2 = read.csv(m2_path, sep = '\t', header = FALSE)
-          if (sample1 == "HB2_WT-1" && sample2 == "HB2_WT-2") {
-            # Calculate optimal smoothing parameter for a given region and bin
-            print(
-              paste('Calculating optimal smoothing parameter for region: ', region, ', bin: ', bin, '.', 
-              sep = ""))
+          # Calculate optimal smoothing parameter for a given region and bin
+          print(
+            paste('Calculating optimal smoothing parameter for region: ', region, ', bin: ', bin, '.', sep = ""))
             tryCatch({h_hat <- htrain(m1, m2, 
                                       resol = bin, 
                                       max = max_interaction, 
                                       range = 0:20)},
                      error = function(e) {cat("ERROR :",conditionMessage(e), "\n"); h_hat = NULL})
             
-          }
+          
           # h_hat will be NULL if HB2_WT-1 or HB2_WT-2 dont exists for region and bin
           if (is.null(h_hat)) {
             next
@@ -107,10 +113,23 @@ for (region in capture_regions$region) {
           scc_values[nrow(scc_values) + 1,] = list(region, bin, sample1, sample2, h_hat, SCC.out$scc)
           write.csv(scc_values, paste(qc_dir, 'hicrep/hicrep_scc_all.csv', sep = '/'))
         } else {
-          scc_values[nrow(scc_values) + 1,] = list(region, bin, sample1, sample2, NA, 1)
+          scc_values[nrow(scc_values) + 1,] = list(region, bin, sample1, sample2, NA, NA)
         }
       }
     }
+    subset = scc_values[scc_values$region == region & scc_values$binsize == bin, 
+                        c('sample1', 'sample2', 'scc')]
+    if (nrow(subset) == 0) {
+      next
+    }
+    g <- graph.data.frame(subset, directed=FALSE)
+    heatmap = pheatmap(get.adjacency(g, attr="scc", sparse=FALSE),
+                       color = viridis(100), display_numbers = TRUE,
+                       number_color = 'red', 
+                       fontsize = 8, fontsize_number = 10,
+                       angle_col = 0, treeheight_col = 0)
+    ggsave(paste(qc_dir, '/hicrep/hicrep-', region, '-', bin, '.png', sep = ''), 
+           plot = heatmap, width = 8, height = 5)
   }
 }
 write.csv(scc_values, paste(qc_dir, 'hicrep/hicrep_scc_all.csv', sep = '/'))
