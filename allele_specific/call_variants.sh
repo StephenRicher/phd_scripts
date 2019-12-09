55cf219f1fe6b97593cf7187eb3cac9423431478
Stephen Richer <sr467@bath.ac.uk>
2019-12-09 17:31:35 +0000

    Removed hapcompass commands.

--- allele_specific/call_variants.sh
+++ allele_specific/call_variants.sh
@@ -43,7 +43,6 @@ main() {
 
     samtools index -@ "${threads}" "${data_dir}"/"${sample}".replicate_merge.bam
 
-
     # Call variants from merged sample file and quality filter.
     vcf_out="${data_dir}"/"${sample}".sorted.vcf.gz
 
@@ -67,39 +66,6 @@ main() {
             --output-type z
 
     bcftools index "${vcf_out}"
-
-    # Subset VCF by capture region and perform haplotype phasing.
-    while IFS=$'\t' read -r chr start end region; do
-        bcftools view \
-                --output-type v \
-                --regions "${chr}":$(("${start}"+1))-"${end}" \
-                "${vcf_out}" \
-            > "${data_dir}"/"${sample}"_"${region}".vcf
-        java -Xmx100g -jar "${hapcompass}" \
-            --bam "${data_dir}"/"${sample}".replicate_merge.bam \
-            --vcf "${data_dir}"/"${sample}"_"${region}".vcf \
-            --debug \
-            --output "${data_dir}"/"${sample}"_"${region}"_hapcompass
-    done <"${capture_regions}"
-
-    MWER_solution_all="${data_dir}/${sample}_all_hapcompass_MWER_solution.txt"
-    rm "${MWER_solution_all}"
-    while IFS=  read -r -d $'\0' file; do
-        # Get line number of header of top scoring section
-        top_score_line=$(grep -n BLOCK "${file}" | sort -k 6 -nr | cut -f 1 -d ':' | head -n 1)
-        # Print out only the top scoring block
-        awk -v n=${top_score_line} 'NR<n {next} NR==n {print;next} /^BLOCK/ {exit} {print}' ${file} >> ${MWER_solution_all}
-    done < <(find "${data_dir}" -type f -name ""${sample}"_*_hapcompass_MWER_solution.txt" -print0)
-
-    # Convert solution to VCF for genome masking
-    java -jar "${hc2vcf}" "${MWER_solution_all}" \
-        <(zcat "${vcf_out}") 2 true
-
-    # Convert VCF to SNPsplit friendly output
-    awk -v OFS=$'\t' 'substr($10,1,3)=="0|1" {print $3, $1, $2, 1, $4"/"$5} substr($10,1,3)=="1|0" {print $3, $1, $2, 1, $5"/"$4}' \
-        "${MWER_solution_all}".vcf \
-        > "${data_dir}"/"${sample}"_MWER_solution_snpsplit.txt
-
 }
 
 
