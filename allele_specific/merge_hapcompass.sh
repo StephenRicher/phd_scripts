c4f31ec74564e426027d5f6b5a88d613193494f9
Stephen Richer <sr467@bath.ac.uk>
2019-12-10 10:51:54 +0000

    Script for merging across regions.

---
+++ allele_specific/merge_hapcompass.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+main() {
+    local samples=( 'HB2_CL4' 'HB2_WT' 'MCF7' )
+    data_dir="/home/u/sr467/scratch/projects/hic-01/allele_specific"
+
+    for sample in "${samples[@]}"; do
+        cat "${data_dir}"/"${sample}"*snpsplit* \
+            > "${data_dir}"/"${sample}"_all_MWER_solution_snpsplit.txt
+
+        for file in MCF7*MWER*vcf; do
+            bcftools sort -O v "$file}" \
+                > "${file/solution/sorted}"
+            bcftools index "${file/solution/sorted}"
+        done
+
+
+        #bcftools "${data_dir}"/"${sample}"-*-MWER.sorted.vcf
+
+    done
+
+}
+
+main
