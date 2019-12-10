441e595581319d66d7b536c39129449b1442f74d
Stephen Richer <sr467@bath.ac.uk>
2019-12-10 11:37:16 +0000

    Add VCF compressed and rename file syntax.

--- allele_specific/run_hapcompass.sh
+++ allele_specific/run_hapcompass.sh
@@ -40,15 +40,15 @@ main() {
             --output-type v \
             --regions "${chr}":$((${start}+1))-"${end}" \
             "${vcf}" \
-        > "${data_dir}"/"${sample}"_"${region}".vcf
+        > "${data_dir}"/"${sample}"-"${region}".vcf
     java -Xmx100g -jar "${hapcompass}" \
         --bam "${data_dir}"/"${sample}".replicate_merge.bam \
-        --vcf "${data_dir}"/"${sample}"_"${region}".vcf \
+        --vcf "${data_dir}"/"${sample}"-"${region}".vcf \
         --debug \
-        --output "${data_dir}"/"${sample}"_"${region}"_hapcompass
+        --output "${data_dir}"/"${sample}"-"${region}"-hapcompass
 
-    local MWER="${data_dir}"/"${sample}"_"${region}"_hapcompass_MWER_solution.txt
-    local MWER_top=""${data_dir}"/"${sample}"_"${region}"_MWER_solution.txt"
+    local MWER="${data_dir}"/"${sample}"-"${region}"-MWER_solution.txt
+    local MWER_best=""${data_dir}"/"${sample}"-"${region}"-MWER_best.txt"
 
     # Get line number of header of top scoring section
     local top_score_line=$(grep -n BLOCK "${MWER}" \
@@ -60,17 +60,20 @@ main() {
     awk -v n="${top_score_line}" '
             NR<n {next} NR==n {print;next} /^BLOCK/ {exit} {print}' \
         "${MWER}" \
-        > "${MWER_top}"
+        > "${MWER_best}"
 
     # Convert solution to VCF for genome masking
-    java -jar "${hc2vcf}" "${MWER_top}" <(zcat "${vcf}") 2 true
+    java -jar "${hc2vcf}" "${MWER_best}" <(zcat "${vcf}") 2 true
 
     # Convert VCF to SNPsplit friendly output
     awk -v OFS=$'\t' '
             substr($10,1,3)=="0|1" {print $3, $1, $2, 1, $4"/"$5}
             substr($10,1,3)=="1|0" {print $3, $1, $2, 1, $5"/"$4}' \
-        "${MWER_top}".vcf \
-        > "${data_dir}"/"${sample}"_"${region}"_MWER_solution_snpsplit.txt
+            "${MWER_best}".vcf \
+        > "${data_dir}"/"${sample}"-"${region}"-MWER_snpsplit.txt
+
+    # Compress VCF
+    bcftools view -O z "${MWER_best}".vcf > "${MWER_best}".vcf.gz
 }
 
 
